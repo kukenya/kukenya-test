74b10317f57f42a934e3e7349e047a487c0a046e
kukenya <jbs7917@gmail.com>
2023-05-31 16:17:43 +0900

    soutv 사용해서 System.out.println 작성해보기

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,7 @@
 public class Application {
     public static void main(String[] args){
         System.out.println("hello world");
+        String a = "몰?루";
+        System.out.println("a = " + a);
     }
 }
